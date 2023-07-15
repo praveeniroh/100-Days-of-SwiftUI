906f846f427888577f4e11d6dccfb86d3739fdec
praveenraj.ta <prav7598@gmail.com>
2023-07-15 16:34:28 +0530

    100days of swift new folder created

---
+++ Day 20_stacks/Day 20_stacks/Day_20_stacksApp.swift
@@ -0,0 +1,17 @@
+//
+//  Day_20_stacksApp.swift
+//  Day 20_stacks
+//
+//  Created by Praveenraj T on 15/07/23.
+//
+
+import SwiftUI
+
+@main
+struct Day_20_stacksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
